b199deea6a7015ce4b72e3980005c53185b54238
rob-pasc <robinpascal.k@gmail.com>
2026-03-03 20:34:58 +0100

    Add generic folders

---
+++ backend/Api/Application/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+using LectureEvaluationAPI.Domain.Entities;
+
+namespace LectureEvaluationAPI.Application.Repositories;
+
+public interface IRepository<TEntity> where TEntity : class, IEntity
+{
+    Task<List<TEntity>> FindAllAsync();
+
+    Task<TEntity?> FindByIdAsync(int id);
+
+    Task<TEntity> AddAsync(TEntity entity);
+
+    Task<TEntity> UpdateAsync(TEntity entity);
+    
+    Task<TEntity> DeleteAsync(TEntity entity);
+}
---
+++ backend/Api/Infrastructure/Repositories/PostgresRepository/PostgresDbContext.cs
@@ -0,0 +1,11 @@
+﻿using LectureEvaluationAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LectureEvaluationAPI.Infrastructure.Repositories.PostgresRepository;
+
+public class PostgresDbContext(DbContextOptions<PostgresDbContext> options) 
+    : DbContext(options)
+{
+    public DbSet<Lecture> Lectures { get; set; }
+    public DbSet<Evaluation> Evaluations { get; set; }
+}
