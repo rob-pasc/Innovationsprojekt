a93049c0cf0ed75e693eb39b614c13274b63e39e
rob-pasc <robinpascal.k@gmail.com>
2026-03-16 19:39:10 +0100

    Update RegisterPage.tsx

--- frontend/src/pages/auth/RegisterPage.tsx
+++ frontend/src/pages/auth/RegisterPage.tsx
@@ -38,6 +38,7 @@ export default function RegisterPage() {
   const passwordValue = watch('password');
 
   const onSubmit = async (data: RegisterFormData) => {
+    alert(import.meta.env.API_URL + '/auth/registe');  //debug
     setApiError(null);
     setAuthLoading(true);
 
